be971758e90d61c0af51d46100c88c6ca34c0f6d
Titouan Petit <petit.titouan1@gmail.com>
2022-04-24 22:05:40 +0200

    Do Some Commits

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const { parentPort } = require('worker_threads')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('WORKING on ' + process.env.ENV)
+})
+
+parentPort.on('message', (msg) => {
+    if (msg === 'stop') {
+        require('fs').writeFileSync(
+            '../../STOP+' + process.env.ENV + '.txt',
+            'STOPPED'
+        )
+    }
+})
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    parentPort.postMessage('ready')
+})
