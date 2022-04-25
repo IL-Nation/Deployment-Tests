b04d8bbdd839dd9d8de65d7387bddf5c8866b31c
Titouan Petit <petit.titouan1@gmail.com>
2022-04-25 21:56:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-require('dotenv').config({
-    override: true,
-    path: './.env',
-})
+require('dotenv').config()
 
 console.log(__dirname)
 console.log(process.env.EXPRESS_PORT)
-console.log(process.env.ENV)
+console.log(process.env)
 
 const { parentPort } = require('worker_threads')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     res.send('WORKING on ' + process.env.ENV)
 })
 
